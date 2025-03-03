cbbdc0cc724ec09356c0d7017a4b196cda0d981c
Krontiva <info@krontiva.africa>
2025-03-03 12:50:55 +0000

    Update Sign In Content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,6 @@ export default function Home() {
       // Authenticate the user
       const loginResponse = await fetch('https://api-server.krontiva.africa/api:uEBBwbSs/auth/login', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body: JSON.stringify(credentials)
       });
 
